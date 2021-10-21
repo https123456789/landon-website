e95708c88e120cdd28e06517c17d92d7006e56cd
https123456789 <familylandon01@outlook.com>
2021-10-20 17:13:51 -0700

    Worked on topic sorting.

--- forum/views.py
+++ forum/views.py
@@ -10,7 +10,7 @@ import globals
 def index(request):
 	return render(request, "forum/index.html")
 def topics(request):
-	topics = Topic.objects.order_by("date_added")
+	topics = Topic.objects.all().sort(reverse = True)
 	context = {
 		"topics": topics
 	}
